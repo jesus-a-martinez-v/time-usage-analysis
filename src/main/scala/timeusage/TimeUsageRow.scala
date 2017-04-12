d4b05439cc53fe368c014bfabf04202fb046fe9a
Jesús Martínez <jesus.a.martinez.v@gmail.com>
2017-04-12 00:29:19 -0500

    First commit

---
+++ src/main/scala/timeusage/TimeUsageRow.scala
@@ -0,0 +1,18 @@
+package timeusage
+
+
+/**
+  * Models a row of the summarized data set
+  * @param working Working status (either "working" or "not working")
+  * @param sex Sex (either "male" or "female")
+  * @param age Age (either "young", "active" or "elder")
+  * @param primaryNeeds Number of daily hours spent on primary needs
+  * @param work Number of daily hours spent on work
+  * @param other Number of daily hours spent on other activities
+  */
+case class TimeUsageRow(working: String,
+                        sex: String,
+                        age: String,
+                        primaryNeeds: Double,
+                        work: Double,
+                        other: Double)
